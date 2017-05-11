6f441050ddc7e0e0fff54886cb29df51be5314ee
Angular CLI <angular-cli@angular.io>
2017-05-11 14:35:56 +0430

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { NgxPanesPage } from './app.po';
+
+describe('ngx-panes App', () => {
+  let page: NgxPanesPage;
+
+  beforeEach(() => {
+    page = new NgxPanesPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

1c82aa6f059f38d967e529f4db9cf762b3a85037
Alireza Mirian <alireza.mirian@gmail.com>
2017-08-11 17:57:55 +0430

    dev: start adding api-docs to docs site

--- website/src/app/with-sidenav/with-sidenav.component.ts
+++ website/src/app/with-sidenav/with-sidenav.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {GuideModel, guides} from '../guide/guides';
+
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 // workaround for importing all guides
 import '../guide/guides/index';
-
 
 @Component({
   selector: 'app-with-sidenav',
@@ -10,13 +12,19 @@ import '../guide/guides/index';
   styleUrls: ['./with-sidenav.component.scss']
 })
 export class WithSidenavComponent implements OnInit {
+  apiDocs$: Observable<any>;
   guides: GuideModel[];
 
-  constructor() {
+  constructor(private http: Http) {
   }
 
   ngOnInit() {
     this.guides = guides;
+    // TODO: extract a service for apiDocs
+    this.apiDocs$ = this.http.get('assets/api-docs.json').map(response => {
+      const apiDocs = response.json();
+      return Object.keys(apiDocs).map(directiveName => ({...apiDocs[directiveName], type: 'directive'}));
+    });
   }
 
 }

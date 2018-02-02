0268936be73367ad85740fce418b972e673e8854
Alireza Mirian <alireza.mirian@gmail.com>
2018-02-02 16:09:31 +0330

    improvement(website): show target section of doc pages, other improvements

--- website/src/app/shared/side-anchor/side-anchor.component.ts
+++ website/src/app/shared/side-anchor/side-anchor.component.ts
@@ -1,15 +1,21 @@
-import {Component, Optional} from '@angular/core';
+import {AfterViewInit, Component, HostBinding, Optional} from '@angular/core';
 import {SideAnchorOwnerDirective} from './side-anchor-owner.directive';
 import {Location} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-side-anchor',
   templateUrl: './side-anchor.component.html',
   styleUrls: ['./side-anchor.component.scss']
 })
-export class SideAnchorComponent {
+export class SideAnchorComponent implements AfterViewInit {
 
-  constructor(@Optional() public sideAnchorOwner: SideAnchorOwnerDirective, private location: Location) {
+  @HostBinding('class.target')
+  target: boolean;
+
+  constructor(@Optional() public sideAnchorOwner: SideAnchorOwnerDirective,
+              private route: ActivatedRoute,
+              private location: Location) {
     if (!sideAnchorOwner) {
       throw new Error('SideAnchorComponent is used without any descendant element having an id');
     }
@@ -19,5 +25,12 @@ export class SideAnchorComponent {
 
   getPath() {
     return this.location.path(false);
+  }
+
+  ngAfterViewInit(): void {
+    this.target = this.route.snapshot.fragment === this.sideAnchorOwner.id;
+    this.route.fragment.subscribe(fragment => {
+      this.target = fragment === this.sideAnchorOwner.id;
+    });
   }
 }

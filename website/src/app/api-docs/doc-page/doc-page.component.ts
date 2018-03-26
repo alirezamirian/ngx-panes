15a3fd511cf13aa41c585642cf0266bc1e821237
Alireza Mirian <alireza.mirian@gmail.com>
2018-03-27 00:30:54 +0430

    refactor: move anchor link tracking to appComponent
    This gets anchor links in components other than ApiDocs to work

--- website/src/app/api-docs/doc-page/doc-page.component.ts
+++ website/src/app/api-docs/doc-page/doc-page.component.ts
@@ -1,14 +1,12 @@
-import {AfterViewInit, Component, HostListener, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {Component, HostListener, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {first} from 'rxjs/operators';
-import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-doc-page',
   templateUrl: './doc-page.component.html',
   styleUrls: ['./doc-page.component.scss']
 })
-export class DocPageComponent implements OnInit, AfterViewInit {
+export class DocPageComponent implements OnInit {
   public docItem: any;
 
   constructor(private route: ActivatedRoute, private router: Router, @Inject(PLATFORM_ID) private platformId) {
@@ -18,14 +16,7 @@ export class DocPageComponent implements OnInit, AfterViewInit {
     this.route.data.subscribe(data => {
       if (data.docItem) {
         this.docItem = data.docItem[0];
-        setTimeout(() => this.syncFragmentScroll(this.route.snapshot.fragment));
       }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    this.route.fragment.subscribe(fragment => {
-      this.syncFragmentScroll(fragment);
     });
   }
 
@@ -33,26 +24,10 @@ export class DocPageComponent implements OnInit, AfterViewInit {
   handleLinkClicks(event: any) {
     if (event.target.tagName.toLowerCase() === 'a') {
       // note that event.target.href is a property which is different with href attribute. href prop is always absolute
-      let href = event.target.getAttribute('href');
+      const href = event.target.getAttribute('href');
       if (href.indexOf('http') !== 0) {
         event.preventDefault();
         this.router.navigateByUrl(href);
-      }
-    }
-  }
-
-  private syncFragmentScroll(fragment: string) {
-    if (isPlatformBrowser(this.platformId) && fragment) {
-      const elem = document.querySelector('#' + fragment);
-      if (elem) {
-        this.router.events.pipe(first()).subscribe(event => {
-          setTimeout(() => {
-            elem.scrollIntoView();
-          });
-        });
-        setTimeout(() => {
-          elem.scrollIntoView();
-        });
       }
     }
   }
--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,14 +1,43 @@
-import {Component, DoCheck} from '@angular/core';
+import {Component, DoCheck, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {isPlatformBrowser} from '@angular/common';
+import {filter, first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements DoCheck {
+export class AppComponent implements DoCheck, OnInit {
+
+  constructor(private route: ActivatedRoute, private router: Router, @Inject(PLATFORM_ID) private platformId) {
+
+  }
 
   ngDoCheck(): void {
     // console.log('cheeeeeeck');
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      setTimeout(() => this.syncFragmentScroll(this.route.snapshot.fragment));
+    });
+  }
+
+  private syncFragmentScroll(fragment: string) {
+    if (isPlatformBrowser(this.platformId) && fragment) {
+      const elem = document.querySelector('#' + fragment);
+      if (elem) {
+        this.router.events.pipe(first()).subscribe(event => {
+          setTimeout(() => {
+            elem.scrollIntoView();
+          });
+        });
+        setTimeout(() => {
+          elem.scrollIntoView();
+        });
+      }
+    }
   }
 
 }
